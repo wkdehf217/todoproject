d7c8ebe117f10f891960f92df2bb6a3e5f664cd3
wkdehf217 <wkdehfdl17@naver.com>
2023-11-17 15:46:51 +0900

    init

---
+++ src/main/java/com/todoproject/todoproject/TodoprojectApplication.java
@@ -0,0 +1,17 @@
+package com.todoproject.todoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
+public class TodoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoprojectApplication.class, args);
+	}
+
+}
